bc4f87c46902f0543541840bf890dfcc830eff58
Rafakimanja <rafaellopes.ifrs@gmail.com>
2023-06-19 21:14:55 -0300

    novas funcionalidades

---
+++ front-end/Js/slider.js
@@ -0,0 +1,21 @@
+
+let radio = document.querySelector('.manual-btn')
+
+let cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 4000)
+
+function proximaImg(){
+    
+    cont++
+
+    if(cont>3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
